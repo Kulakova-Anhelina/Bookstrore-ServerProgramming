1facaa62209a3b69055ddb0bf212a5bd2d10f5cd
Kulakova-Anhelina <kulakovaanhelina@gmail.com>
2020-03-31 19:02:06 +0200

    smoke testing

--- BookstoreDatabase/src/test/java/fi/haagahelia/BookstoreDatabase/BookstoreDatabaseApplicationTests.java
+++ BookstoreDatabase/src/test/java/fi/haagahelia/BookstoreDatabase/BookstoreDatabaseApplicationTests.java
@@ -2,15 +2,21 @@ package fi.haagahelia.BookstoreDatabase;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class BookstoreDatabaseApplicationTests {
+	@Autowired
+	private BookstoreDatabaseApplication controller;
 
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
